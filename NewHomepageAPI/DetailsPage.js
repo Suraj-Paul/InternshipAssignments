56c137c559a7100a429e2180707f9b1d1fb984f7
Suraj Paul <67500086+Suraj-Paul@users.noreply.github.com>
2020-08-24 17:17:45 +0530

    Update DetailsPage.js

--- NewHomepageAPI/DetailsPage.js
+++ NewHomepageAPI/DetailsPage.js
@@ -5,18 +5,23 @@ const mongo = require('mongodb');
 const MongoClint = mongo.MongoClient;
 const mongourl = "mongodb://localhost:27017";
 let db;
-let coll_name = "project"
+let coll_name = "sampleDetails";
 
 app.get('/',(req,res) => {
     res.status(200).send('This is homepage')
 })
+//fetching details
+app.get('/details',(req,res) => {
+    db.collection(coll_name).find().toArray((err,result) =>{
+        if(err) throw err;
+        res.send(result)
+    })
+})
 
-
-
-
+//Database connectivity
 MongoClint.connect(mongourl,(err,client) => {
     if(err) console.log(err);
-    db = client.db('SampleDetails')
+    db = client.db('project')
     app.listen(port,(err) => {
         if(err) throw err;
         console.log(`Server is running on port no. ${port}`)
